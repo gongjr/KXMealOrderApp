3f28ecd2317ff7bc4d2446c554185168afdc3ba2
hlyao <skynight.yao@foxmail.com>
2016-03-23 16:18:30 +0800

    111

--- src/com/asiainfo/mealorder/entity/HurryOrderDesk.java
+++ src/com/asiainfo/mealorder/entity/HurryOrderDesk.java
@@ -7,4 +7,14 @@ package com.asiainfo.mealorder.entity;
  * @version V1.0, 16/3/23 下午2:23
  */
 public class HurryOrderDesk {
+
+    private String deskName;
+
+    public String getDeskName() {
+        return deskName;
+    }
+
+    public void setDeskName(String deskName) {
+        this.deskName = deskName;
+    }
 }
