48e50189dfddf8ac2235eac5eb6f56c58b94c603
hlyao <skynight.yao@foxmail.com>
2016-05-09 17:34:20 +0800

    修改订单页面样式

---
+++ src/com/asiainfo/mealorder/adapter/OrderAdapter.java
@@ -0,0 +1,8 @@
+package com.asiainfo.mealorder.adapter;
+
+/**
+ * @author skynight(skynight@dingtalk.com)
+ * @creatTime 16/5/6 下午5:52
+ */
+public class OrderAdapter {
+}
---
+++ src/com/asiainfo/mealorder/ui/OrderActivity.java
@@ -0,0 +1,19 @@
+package com.asiainfo.mealorder.ui;
+
+import android.os.Bundle;
+
+import com.asiainfo.mealorder.R;
+import com.asiainfo.mealorder.ui.base.BaseActivity;
+
+/**
+ * @author skynight(skynight@dingtalk.com)
+ * @creatTime 16/5/6 下午4:19
+ */
+public class OrderActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_order);
+    }
+}
